5da1eb3e5ad70f971eeb60d5b7f1c5ceeb3869fb
Nick Porillo <nsporillo@gmail.com>
2018-08-17 23:37:51 -0400

    Skeleton Project

---
+++ src/main/java/GlobalWarming.java
@@ -0,0 +1,14 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GlobalWarming extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
