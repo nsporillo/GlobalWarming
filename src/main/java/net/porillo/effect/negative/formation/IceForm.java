f32978206074fc89a2855fd3d5b26cfb0e3d9ddc
Diego Arias <dariascauas@gmail.com>
2018-09-12 20:37:15 -0300

    Misc Effect fixes

--- src/main/java/net/porillo/effect/negative/formation/IceForm.java
+++ src/main/java/net/porillo/effect/negative/formation/IceForm.java
@@ -1,28 +1,28 @@
 package net.porillo.effect.negative.formation;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import net.porillo.GlobalWarming;
+import net.porillo.effect.ClimateData;
+import net.porillo.effect.api.ClimateEffectType;
 import net.porillo.effect.api.ListenerClimateEffect;
 import net.porillo.engine.ClimateEngine;
-import net.porillo.util.MapUtil;
+import net.porillo.engine.api.Distribution;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockFormEvent;
 
-import java.util.TreeMap;
-
+@ClimateData(type = ClimateEffectType.ICE_FORMATION)
 public class IceForm extends ListenerClimateEffect {
 
-    private TreeMap<Double, Integer> heightMap;
+    private Distribution heightMap;
 
     @EventHandler
     public void blockFormEvent(BlockFormEvent event) {
-        if (event.getBlock().getType() == Material.SNOW) {
+        if (event.getNewState().getType() == Material.ICE) {
             double temp = ClimateEngine.getInstance().getClimateEngine(event.getBlock().getWorld().getName()).getTemperature();
 
-            if (event.getBlock().getY() < MapUtil.searchTreeMap(heightMap, temp)) {
+            if (event.getBlock().getY() < heightMap.getValue(temp)) {
                 event.setCancelled(true);
             }
         }
@@ -31,7 +31,7 @@ public class IceForm extends ListenerClimateEffect {
     @Override
     public void setJsonModel(JsonObject jsonModel) {
         super.setJsonModel(jsonModel);
-        this.heightMap = GlobalWarming.getInstance().getGson().fromJson(jsonModel, new TypeToken<TreeMap<Integer, Integer>>(){}.getType());
+        this.heightMap = GlobalWarming.getInstance().getGson().fromJson(jsonModel, new TypeToken<Distribution>(){}.getType());
         if (this.heightMap == null) {
             unregister();
         }
--- src/main/java/net/porillo/effect/negative/formation/SnowForm.java
+++ src/main/java/net/porillo/effect/negative/formation/SnowForm.java
@@ -1,27 +1,28 @@
 package net.porillo.effect.negative.formation;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import net.porillo.GlobalWarming;
+import net.porillo.effect.ClimateData;
+import net.porillo.effect.api.ClimateEffectType;
 import net.porillo.effect.api.ListenerClimateEffect;
 import net.porillo.engine.ClimateEngine;
-import net.porillo.util.MapUtil;
+import net.porillo.engine.api.Distribution;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockFormEvent;
 
-import java.util.TreeMap;
-
+@ClimateData(type = ClimateEffectType.SNOW_FORMATION)
 public class SnowForm extends ListenerClimateEffect {
 
-    private TreeMap<Double, Integer> heightMap;
+    private Distribution heightMap;
 
     @EventHandler
     public void blockFormEvent(BlockFormEvent event) {
-        if (event.getBlock().getType() == Material.ICE) {
+        if (event.getNewState().getType() == Material.SNOW) {
             double temp = ClimateEngine.getInstance().getClimateEngine(event.getBlock().getWorld().getName()).getTemperature();
 
-            if (event.getBlock().getY() < MapUtil.searchTreeMap(heightMap, temp)) {
+            if (event.getBlock().getY() < heightMap.getValue(temp)) {
                 event.setCancelled(true);
             }
         }
@@ -30,7 +31,7 @@ public class SnowForm extends ListenerClimateEffect {
     @Override
     public void setJsonModel(JsonObject jsonModel) {
         super.setJsonModel(jsonModel);
-        this.heightMap = new Gson().fromJson(jsonModel, new TypeToken<TreeMap<Double, Integer>>(){}.getType());
+        this.heightMap = GlobalWarming.getInstance().getGson().fromJson(jsonModel, new TypeToken<Distribution>(){}.getType());
         if (this.heightMap == null) {
             unregister();
         }
